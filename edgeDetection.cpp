6e8b9126891ff9dad86b7dc384c7fa7fe1d56894
Davis Peterson <davis.peterson@genesys.com>
2019-11-25 12:45:22 -0500

    add edge detection

--- edgeDetection.cpp
+++ edgeDetection.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    Mat img, imgGrayscale, imgGaussian;
+    Mat img, imgGrayscale, imgGaussian, edges;
 
     cout<<"Reading image" << endl;
     img = imread("../Hallway2.png", 1);
@@ -18,11 +18,13 @@ int main(int argc, char* argv[])
     cvtColor(img, imgGrayscale, COLOR_BGR2GRAY);
     cout<<"Applying Gaussian blur" << endl;
     GaussianBlur(imgGrayscale, imgGaussian, Size(5,5), 0, 0, BORDER_DEFAULT);
+    Canny(imgGaussian, edges, 230, 255, 5, false);
 
     cout<<"Showing processed image" << endl;
     while(true) {
         imshow("Grayscale", imgGrayscale);
         imshow("Gaussian", imgGaussian);
+        imshow("Canny", edges);
         waitKey(50);
     }
 
